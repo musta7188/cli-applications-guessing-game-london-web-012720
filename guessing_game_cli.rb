1cb856fff581ac660cfb0e85a22e1e3377f46133
Moustafa Mohamed <musta7188@hotmail.it>
2020-01-23 15:13:38 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,7 +3,7 @@ def run_guessing_game
   rando = rand(6)+1
   puts "Guess a number between 1 and 6:"
   input = gets.chomp
-  if input == rando
+  if input == rando.to_s
     puts "You guessed the correct number!"
   elsif input == "exit"
     puts "Goodbye!"
